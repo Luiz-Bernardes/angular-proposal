6e42efda8a49ca0ca64e47de4896b8d167cd68fe
Luiz Bernardes <soumaisluiz@hotmail.com>
2019-03-24 14:02:37 -0300

    task component

---
+++ src/app/tasks/tasks.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+const TASKS: Array<any> = [
+	{ id: 1, title: 'tarefa 1' },
+	{ id: 2, title: 'tarefa 2' },
+	{ id: 3, title: 'tarefa 3' },
+	{ id: 4, title: 'tarefa 4' },
+	{ id: 5, title: 'tarefa 5' },
+	{ id: 6, title: 'tarefa 6' },
+	{ id: 7, title: 'tarefa 7' }
+]
+// const TASKS: any[] = []
+
+@Component({
+	selector: 'tasks',
+	templateUrl: './tasks.component.html'
+})
+
+export class TasksComponent implements OnInit{
+	public tasks;
+
+	public ngOnInit(){
+		this.tasks = TASKS;
+	}
+}
