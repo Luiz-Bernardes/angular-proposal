0d5139349fffece8e48fc5964b36cb788cdc1319
Luiz Bernardes <soumaisluiz@hotmail.com>
2019-03-26 19:32:20 -0300

    remove not used task codes

--- src/app/tasks/task-detail/task-detail.component.ts
+++ src/app/tasks/task-detail/task-detail.component.ts
@@ -12,6 +12,8 @@ import { TaskService } from '../shared/task.service';
 })
 
 export class TaskDetailComponent implements OnInit{
+	public task: Task;
+
 	public constructor(
 		private taskService: TaskService,
 		private route: ActivatedRoute
