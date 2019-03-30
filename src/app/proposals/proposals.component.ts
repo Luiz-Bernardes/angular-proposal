ec0c091b7e3bcd234134b766e7f385a6ae476dd4
Luiz Bernardes <soumaisluiz@hotmail.com>
2019-03-30 13:59:57 -0300

    required proposal fields function validation

--- src/app/proposals/proposals.component.ts
+++ src/app/proposals/proposals.component.ts
@@ -6,6 +6,7 @@ import { ProposalService } from './shared/proposal.service'
 @Component({
 	selector: 'proposals',
 	templateUrl: './proposals.component.html',
+	styleUrls: ['../app.component.scss'],
 	providers: [ProposalService]
 })
 
@@ -18,13 +19,7 @@ export class ProposalsComponent implements OnInit{
 	}
 
 	public createProposal(){
-		this.newProposal.name = this.newProposal.name.trim(); 
-		this.newProposal.description = this.newProposal.description.trim(); 
-		this.newProposal.city = this.newProposal.city.trim(); 
-
-		if(!this.newProposal.name){
-			alert("A proposta deve ter um nome!");
-		} else {
+		if(this.validateRequiredFields()){
 			this.proposalService.create(this.newProposal)
 				.subscribe(
 					(proposal) => {
@@ -33,8 +28,22 @@ export class ProposalsComponent implements OnInit{
 					},
 					() => alert("Ocorreu um erro no servidor, tente mais tarde!")
 				)
+		} else {
+			alert("Os campos devem ser preenchidos!");
 		}
+	}
 
+	public validateRequiredFields(){
+		this.newProposal.name = this.newProposal.name.trim(); 
+		this.newProposal.description = this.newProposal.description.trim(); 
+		this.newProposal.city = this.newProposal.city.trim(); 
+
+		if(!this.newProposal.name){ return false; } 
+		else if (!this.newProposal.description){ return false; }
+		else if (!this.newProposal.date_begin){ return false; }
+		else if (!this.newProposal.date_end){ return false; }
+		else if (!this.newProposal.city){ return false; }
+		else { return true; }
 	}
 
 	// public deleteTask(task: Task){
